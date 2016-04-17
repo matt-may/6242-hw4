ec4946d1eaa5cb29c284d1a1168697bb3c69e1e2
Matt May <matthewbentonmay@gmail.com>
2016-04-17 15:14:01 -0500

    coming along...

--- skel/hw4-skeleton/Task1/RandomForest.py
+++ skel/hw4-skeleton/Task1/RandomForest.py
@@ -20,28 +20,72 @@ myname = "May-Matthew"
 class RandomForest(object):
     class __DecisionTree(object):
         tree = {}
+        M = 3
 
         def learn(self, X, y):
             # TODO: train decision tree and store it in self.tree
-            print(y)
-            print Utils.entropy(np.array([1,0,0,0,0,1,1]))
-            print Utils.shuffle(np.array([1,2]), np.array([3,4]))
-            exit()
-            # self.tree = self.build_tree(X, y)
+            self.num_features = X.shape[1]
+
+            indices = np.random.choice(self.num_features, size=self.M, replace=False)
+            self.tree = self.build_tree(X, y, indices, 0)
 
         def classify(self, test_instance):
             # TODO: return predicted label for a single instance using self.tree
             return 0
 
-        def build_tree(self, X, y):
-            pass
+        def build_tree(self, X, y, indices, d):
+            print('Building tree...')
+            if Utils.entropy(y) == 0: return np.bincount(y).argmax()
+
+            indx, thresh = self.best_split(X, y, indices)
+
+        def best_split(self, X, y, indices):
+            gain, fi, thresh = 0, 0, 0
+
+            for i in indices:
+                vals = X[:, i]
+                vals = sorted(set(vals))
+
+                for v in xrange(len(vals) - 1):
+                    new_tresh = (vals[v] + vals[v + 1]) / 2
+
+                    X_1, y_1, X_2, y_2 = self.split(X, y, i, new_tresh)
+
+                    new_gain = Utils.gain(y, y_1, y_2)
+
+                    if new_gain > gain:
+                        gain, fi, thresh = new_gain, i, new_tresh
+
+            return fi, thresh
+
+        def split(self, X, y, fi, thresh):
+            """
+            Splits both features and labels into two groups each, based on a
+            defined threshold.
+
+            """
+
+            less_than = np.where(X[:,fi] <= thresh)
+            greater_than = np.where(X[:,fi] > thresh)
+
+            # Features and labels less than or equal to the threshold.
+            X_1 = X[less_than]
+            y_1 = y[less_than]
+
+            # Features and labels greater than the threshold.
+            X_2 = X[greater_than]
+            y_2 = y[greater_than]
+
+            return X_1, y_1, X_2, y_2
 
     decision_trees = []
 
     def __init__(self, num_trees):
         # TODO: do initialization here, you can change the function signature according to your need
         self.num_trees = num_trees
+        print('hello')
         self.decision_trees = [self.__DecisionTree()] * num_trees
+        print('done')
 
     # You MUST NOT change this signature
     def fit(self, X, y):
@@ -100,15 +144,17 @@ class Utils(object):
         """
 
         def weighted_entropy(child, n_labs):
-            return len(child) / n_labs * entropy(child)
+            return len(child) / n_labs * Utils.entropy(child)
 
         n_labs = len(parent)
-        return entropy(parent) - \
+        return Utils.entropy(parent) - \
                 (weighted_entropy(child1, n_labs) + weighted_entropy(child2, n_labs))
 
 def main():
     X = []
     y = []
+
+    print('here')
 
     # Load data set
     with open("hw4-data.csv") as f:
@@ -118,8 +164,12 @@ def main():
             X.append(line[:-1])
             y.append(line[-1])
 
+    print('here2')
+
     X = np.array(X, dtype = float)
     y = np.array(y, dtype = int)
+
+    print('here3')
 
     # Split training/test sets
     # You need to modify the following code for cross validation
@@ -129,9 +179,15 @@ def main():
     X_test  = np.array([x for i, x in enumerate(X) if i % K == 9], dtype = float)
     y_test  = np.array([z for i, z in enumerate(y) if i % K == 9], dtype = int)
 
-    randomForest = RandomForest(999)  # Initialize according to your implementation
+    print('here4')
+
+    randomForest = RandomForest(32)  # Initialize according to your implementation
+
+    print('here5')
 
     randomForest.fit(X_train, y_train)
+
+    print('here6')
 
     y_predicted = randomForest.predict(X_test)
 
